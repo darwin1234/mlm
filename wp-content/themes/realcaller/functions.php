09e2414dd6ead2b47d53365070fdb7be7f14c2f1
Darwin SEse <darwinsese@gmail.com>
2025-03-06 21:21:27 +0800

    fix redirect

--- wp-content/themes/realcaller/functions.php
+++ wp-content/themes/realcaller/functions.php
@@ -83,7 +83,7 @@ class dsMLM {
 		}
 		$current_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-		add_action('template_redirect', array($this,'custom_redirect_my_account_page'));
+		//add_action('template_redirect', array($this,'custom_redirect_my_account_page'));
 	}	
 
 
