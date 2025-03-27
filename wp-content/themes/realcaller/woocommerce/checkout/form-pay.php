9a5a616184c8ff2a672fb9367f1dd7c6351ede91
Darwin SEse <darwinsese@gmail.com>
2025-03-28 02:02:19 +0800

    Invoice Update

--- wp-content/themes/realcaller/woocommerce/checkout/form-pay.php
+++ wp-content/themes/realcaller/woocommerce/checkout/form-pay.php
@@ -19,91 +19,173 @@ defined( 'ABSPATH' ) || exit;
 
 $totals = $order->get_order_item_totals(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 ?>
-<form id="order_review" method="post">
-
-	<table class="shop_table">
-		<thead>
-			<tr>
-				<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-				<th class="product-quantity"><?php esc_html_e( 'Qty', 'woocommerce' ); ?></th>
-				<th class="product-total"><?php esc_html_e( 'Totals', 'woocommerce' ); ?></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php if ( count( $order->get_items() ) > 0 ) : ?>
-				<?php foreach ( $order->get_items() as $item_id => $item ) : ?>
-					<?php
-					if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
-						continue;
-					}
-					?>
-					<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'order_item', $item, $order ) ); ?>">
-						<td class="product-name">
-							<?php
-								echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false ) );
-
-								do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order, false );
-
-								wc_display_item_meta( $item );
-
-								do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $order, false );
-							?>
-						</td>
-						<td class="product-quantity"><?php echo apply_filters( 'woocommerce_order_item_quantity_html', ' <strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', esc_html( $item->get_quantity() ) ) . '</strong>', $item ); ?></td><?php // @codingStandardsIgnoreLine ?>
-						<td class="product-subtotal"><?php echo $order->get_formatted_line_subtotal( $item ); ?></td><?php // @codingStandardsIgnoreLine ?>
-					</tr>
-				<?php endforeach; ?>
-			<?php endif; ?>
-		</tbody>
-		<tfoot>
-			<?php if ( $totals ) : ?>
-				<?php foreach ( $totals as $total ) : ?>
-					<tr>
-						<th scope="row" colspan="2"><?php echo $total['label']; ?></th><?php // @codingStandardsIgnoreLine ?>
-						<td class="product-total"><?php echo $total['value']; ?></td><?php // @codingStandardsIgnoreLine ?>
-					</tr>
-				<?php endforeach; ?>
-			<?php endif; ?>
-		</tfoot>
-	</table>
-
-	<?php
-	/**
-	 * Triggered from within the checkout/form-pay.php template, immediately before the payment section.
-	 *
-	 * @since 8.2.0
-	 */
-	do_action( 'woocommerce_pay_order_before_payment' ); 
-	?>
-
-	<div id="payment">
-		<?php if ( $order->needs_payment() ) : ?>
-			<ul class="wc_payment_methods payment_methods methods">
-				<?php
-				if ( ! empty( $available_gateways ) ) {
-					foreach ( $available_gateways as $gateway ) {
-						wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
-					}
-				} else {
-					echo '<li>';
-					wc_print_notice( apply_filters( 'woocommerce_no_available_payment_methods_message', esc_html__( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) ), 'notice' ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
-					echo '</li>';
-				}
-				?>
-			</ul>
-		<?php endif; ?>
-		<div class="form-row">
-			<input type="hidden" name="woocommerce_pay" value="1" />
-
-			<?php wc_get_template( 'checkout/terms.php' ); ?>
-
-			<?php do_action( 'woocommerce_pay_order_before_submit' ); ?>
-
-			<?php echo apply_filters( 'woocommerce_pay_order_button_html', '<button type="submit" class="button alt' . esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ) . '" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
-
-			<?php do_action( 'woocommerce_pay_order_after_submit' ); ?>
-
-			<?php wp_nonce_field( 'woocommerce-pay', 'woocommerce-pay-nonce' ); ?>
+<style>
+	.woocommerce-error{
+		display:none;
+	}
+	#ds_invoice{
+		border:1px solid #ccc;
+		padding-top:50px;
+		padding:10px;
+	}
+</style>
+<div id="ds_invoice" class="container">
+	<div class="row">
+		  <div class="col-md-12">
+			<h3 class="text-end">INVOICE</h3>
+			<hr>
 		</div>
 	</div>
-</form>
+	<div class="row">
+		<div class="col-md-6">
+			<img style="width:200px;" src="https://msgsndr-private.storage.googleapis.com/locationPhotos/d0e7e13b-90c1-4d7d-9a30-5ed4a598c9ee.png">
+		</div>
+		<div class="col-md-6">
+			 <p  class="text-end">Dealsdpt inc. O/A MarketingDPT</p>
+			 <p  class="text-end">(416) 410-7552</p>
+			 <p  class="text-end">1135 Stellar Dr</p>
+			 <p  class="text-end">Newmarket, ON L3Y 7B8</p>
+			 <p  class="text-end">Canada</p>
+		</div>
+		<div class="col-md-12">
+			<hr>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8">
+			<table class="table">
+				<tr>
+					<td><strong>Billed to</strong<</td>
+					<td><strong>Invoice No</strong></td>
+					<td><strong>Issue Date</strong></td>
+				</tr>
+				<tr>
+					<td>
+						<p>Darwin Sese</p>
+						<p>darwinsese@gmail.com</p>
+						<p>Canada</p>
+					</td>
+					<td>
+						INV-000040
+					</td>
+					<td>
+					March 27, 2025
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div class="col-md-4">
+			
+			<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="width:100%; background-color:#2EAD2E; border-color:#2EAD2E;">
+			  Pay 
+			</button>
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+		<form id="order_review" method="post">
+			<table class="shop_table">
+				<thead>
+					<tr>
+						<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th> 
+						<th class="product-quantity"><?php esc_html_e( 'Qty', 'woocommerce' ); ?></th>
+						<th class="product-total"><?php esc_html_e( 'Totals', 'woocommerce' ); ?></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php if ( count( $order->get_items() ) > 0 ) : ?>
+						<?php foreach ( $order->get_items() as $item_id => $item ) : ?>
+							<?php
+							if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
+								continue;
+							}
+							?>
+							<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'order_item', $item, $order ) ); ?>">
+								<td class="product-name">
+									<?php
+										echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false ) );
+
+										do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order, false );
+
+										wc_display_item_meta( $item );
+
+										do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $order, false );
+									?>
+								</td>
+								<td class="product-quantity"><?php echo apply_filters( 'woocommerce_order_item_quantity_html', ' <strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', esc_html( $item->get_quantity() ) ) . '</strong>', $item ); ?></td><?php // @codingStandardsIgnoreLine ?>
+								<td class="product-subtotal"><?php echo $order->get_formatted_line_subtotal( $item ); ?></td><?php // @codingStandardsIgnoreLine ?>
+							</tr>
+						<?php endforeach; ?>
+					<?php endif; ?>
+				</tbody>
+				<tfoot>
+					<?php if ( $totals ) : ?>
+						<?php foreach ( $totals as $total ) : ?>
+							<tr>
+								<th scope="row" colspan="2"><?php echo $total['label']; ?></th><?php // @codingStandardsIgnoreLine ?>
+								<td class="product-total"><?php echo $total['value']; ?></td><?php // @codingStandardsIgnoreLine ?>
+							</tr>
+						<?php endforeach; ?>
+					<?php endif; ?>
+				</tfoot>
+			</table>
+
+			<?php
+			/**
+			 * Triggered from within the checkout/form-pay.php template, immediately before the payment section.
+			 *
+			 * @since 8.2.0
+			 */
+			do_action( 'woocommerce_pay_order_before_payment' ); 
+			?>
+
+			<div id="payment">
+				
+
+				<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+					<div class="modal-body">
+					<?php if ( $order->needs_payment() ) : ?>
+					<ul class="wc_payment_methods payment_methods methods">
+						<?php
+						if ( ! empty( $available_gateways ) ) {
+							foreach ( $available_gateways as $gateway ) {
+								wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
+							}
+						} else {
+							echo '<li>';
+							wc_print_notice( apply_filters( 'woocommerce_no_available_payment_methods_message', esc_html__( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) ), 'notice' ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+							echo '</li>';
+						}
+						?>
+					</ul>
+					<?php endif; ?>
+					</div>
+					<div class="modal-footer">
+						<input type="hidden" name="woocommerce_pay" value="1" />
+
+						<?php do_action( 'woocommerce_pay_order_before_submit' ); ?>
+
+						<?php echo apply_filters( 'woocommerce_pay_order_button_html', '<button type="submit" class="button alt' . esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ) . '" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
+
+						<?php do_action( 'woocommerce_pay_order_after_submit' ); ?>
+
+						<?php wp_nonce_field( 'woocommerce-pay', 'woocommerce-pay-nonce' ); ?>
+					</div>
+					</div>
+				</div>
+				</div>
+				<div class="form-row">
+					
+				
+				</div>
+				<?php wc_get_template( 'checkout/terms.php' ); ?>
+			</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+
