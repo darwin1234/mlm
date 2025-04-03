26c1879e8083cf7636b3bd8516f7ab0b76c6f685
Darwin SEse <darwinsese@gmail.com>
2025-04-03 11:22:19 +0800

    User Role

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'realcaller' );
+define( 'DB_NAME', 'staging_realcaller' );
 
 /** MySQL database username */
-define( 'DB_USER', 'realcaller' );
+define( 'DB_USER', 'staging_realcaller' );
 
 /** MySQL database password */
 //define( 'DB_PASSWORD', '' );
